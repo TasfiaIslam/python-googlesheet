c26b353fb75acb3aaf88039d145383fc3696ef86
Tasfia Islam <tasf1a027@gmail.com>
2021-01-15 20:40:54 +0600

    Read data from spreadsheet

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-# import libraries
-import gspread
-import mysql.connector
-from oauth2client.service_account import ServiceAccountCredentials
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+from decouple import config
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+SERVICE_ACCOUNT_FILE = 'keys.json'
+
+creds = None
+creds = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = config('SAMPLE_SPREADSHEET_ID')
+
+
+service = build('sheets', 'v4', credentials=creds)
+
+# Call the Sheets API
+sheet = service.spreadsheets()
+result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                            range="Mar-2016!A3:G46").execute()
+values = result.get('values', [])
+print(values)
